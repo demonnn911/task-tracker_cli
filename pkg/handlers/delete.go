467babedbfdc89ff062c9b911de4212b47b9a76e
demonnn911 <dmit.leush@gmail.com>
2024-11-03 20:28:10 +0300

    refactor structure of code

--- pkg/handlers/delete.go
+++ pkg/handlers/delete.go
@@ -1,5 +1,32 @@
 package handlers
 
-func DeleteTask(arguments []string) {
+import (
+	"encoding/json"
+	"fmt"
+	"task-tracker_cli/models"
+)
 
+func (h *Handler) DeleteTask(arguments []string) error {
+	id, err := getId()
+	if err != nil {
+		return err
+	}
+	var tasks []models.Task
+	decoder := json.NewDecoder(h.storage)
+	if err := decoder.Decode(&tasks); err != nil {
+		return err
+	}
+	index, err := verifyId(id, tasks)
+	if err != nil {
+		return err
+	}
+	tasks = append(tasks[:index], tasks[(index+1):]...)
+	h.storage.Seek(0, 0)
+	h.storage.Truncate(0)
+	encoder := json.NewEncoder(h.storage)
+	if err := encoder.Encode(&tasks); err != nil {
+		return err
+	}
+	fmt.Printf("You succesfully deleted task with id %d\n", id)
+	return nil
 }
--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -1,11 +1,8 @@
 package handlers
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 	"strings"
-	"task-tracker_cli/models"
 )
 
 type Handler struct {
@@ -28,30 +25,5 @@ func (h *Handler) DoAction(arguments []string) error {
 	case "delete":
 		return h.DeleteTask(arguments)
 	}
-	return nil
-}
-
-func (h *Handler) DeleteTask(arguments []string) error {
-	id, err := getId()
-	if err != nil {
-		return err
-	}
-	var tasks []models.Task
-	decoder := json.NewDecoder(h.storage)
-	if err := decoder.Decode(&tasks); err != nil {
-		return err
-	}
-	index, err := verifyId(id, tasks)
-	if err != nil {
-		return err
-	}
-	tasks = append(tasks[:index], tasks[(index+1):]...)
-	h.storage.Seek(0, 0)
-	h.storage.Truncate(0)
-	encoder := json.NewEncoder(h.storage)
-	if err := encoder.Encode(&tasks); err != nil {
-		return err
-	}
-	fmt.Printf("You succesfully deleted task with id %d\n", id)
 	return nil
 }
